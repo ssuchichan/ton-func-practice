deb929faad069b4aa45c7d0b71ca5ada41711b61
ssuchichan <ssuchichan@gmail.com>
2024-08-25 16:53:41 +0800

    deposit, withdraw.

--- 01first_contract/tests/main.spec.ts
+++ 01first_contract/tests/main.spec.ts
@@ -1,16 +1,35 @@
 import { Cell, toNano } from "ton-core";
 import { hex } from "../build/main.compiled.json";
-import { Blockchain } from "@ton-community/sandbox";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton-community/sandbox";
 import { MainContract } from "../wrappers/MainContract";
 import "@ton-community/test-utils";
 
 describe("main.fc contracts tests", () => {
-    it("should get the proper most recent sender address", async() => {
+    let blockchain: Blockchain;
+    let myContract: SandboxContract<MainContract>;
+    let initWallet: SandboxContract<TreasuryContract>;
+    let ownerWallet: SandboxContract<TreasuryContract>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        initWallet = await blockchain.treasury("initWallet");
+        ownerWallet = await blockchain.treasury("ownerWallet");
+
         const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
-        const blockchain = await Blockchain.create();
-        const myContract = blockchain.openContract(await MainContract.createFromConfig({}, codeCell));
+        myContract = blockchain.openContract(
+            await MainContract.createFromConfig({
+                number: 0,
+                address: initWallet.address,
+                ownerAddress: ownerWallet.address,
+            }, 
+            codeCell)
+        );
+    });
+
+
+    it("should successfully increase counter in contract and get the proper most recent sender address", async() => {
         const senderWallet = await blockchain.treasury("sender");
-        const sentMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"));
+        const sentMessageResult = await myContract.sendIncrement(senderWallet.getSender(), toNano("0.05"), 5);
 
         expect(sentMessageResult.transactions).toHaveTransaction({
             from: senderWallet.address,
@@ -21,7 +40,87 @@ describe("main.fc contracts tests", () => {
         const data = await myContract.getData();
 
         expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+        expect(data.number).toEqual(5);
 
+    });
+
+    it("successfully deposits funds", async () => {
+        const senderWallet = await blockchain.treasury("sender");
+        const depositMessageResult = await myContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+        expect(depositMessageResult.transactions).toHaveTransaction({
+            from: senderWallet.address, 
+            to: myContract.address, 
+            success: true
+        });
+
+        const balanceResult = await myContract.getBalance();
+        expect(balanceResult.balance).toBeGreaterThan(toNano("4.99"));
+    });
+
+    it("should return deposit funds as no command is sent", async () => {
+        const senderWallet = await blockchain.treasury("sender");
+        const sendNoCodeDepositResult = await myContract.sendNoCodeDeposit(senderWallet.getSender(), toNano("5"));
+        expect(sendNoCodeDepositResult.transactions).toHaveTransaction({
+            from: senderWallet.address, 
+            to: myContract.address, 
+            success: false
+        });
+
+        const balanceResult = await myContract.getBalance();
+        expect(balanceResult.balance).toEqual(0);
+    });
+
+    it("successfully withdraws funds on behalf of owner", async () => {
+        const senderWallet = await blockchain.treasury("sender");
+        
+        await myContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+
+        const withdrawalResult = await myContract.sendWithdrawalRequest(
+            ownerWallet.getSender(), 
+            toNano("0.05"), 
+            toNano("1")
+        );
+
+        expect(withdrawalResult.transactions).toHaveTransaction({
+            from: myContract.address, 
+            to: ownerWallet.address, 
+            success: true,
+            value: toNano(1)
+        });
+    });
+
+    it("fails to withdraw funds on behalf of non-owner", async () => {
+        const senderWallet = await blockchain.treasury("sender");
+        
+        await myContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+
+        const withdrawalRequestResult = await myContract.sendWithdrawalRequest(
+            senderWallet.getSender(), 
+            toNano("0.05"), 
+            toNano("1")
+        );
+
+        expect(withdrawalRequestResult.transactions).toHaveTransaction({
+            from: senderWallet.address, 
+            to: myContract.address, 
+            success: false,
+            exitCode: 103
+        });
+    });
+
+    it("fails to withdraw funds because lack of balance", async () => {
+        const withdrawalRequestResult = await myContract.sendWithdrawalRequest(
+            ownerWallet.getSender(), 
+            toNano("0.05"), 
+            toNano("1")
+        );
+        
+        expect(withdrawalRequestResult.transactions).toHaveTransaction({
+            from: ownerWallet.address, 
+            to: myContract.address, 
+            success: false,
+            exitCode: 104
+        });
     });
 });
 
--- 01first_contract/wrappers/MainContract.ts
+++ 01first_contract/wrappers/MainContract.ts
@@ -1,6 +1,20 @@
 import {Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode} from "ton-core";
 
-export class MainContract implements Contract{
+export type MainContractConfig = {
+    number: number,
+    address: Address,
+    ownerAddress: Address,
+};
+
+export function mainContractConfigToCell(config: MainContractConfig) : Cell {
+    return beginCell()
+    .storeUint(config.number, 32)
+    .storeAddress(config.address)
+    .storeAddress(config.ownerAddress)
+    .endCell();
+}
+
+export class MainContract implements Contract {
     constructor(
         readonly address: Address, 
         readonly init?: {code: Cell, data: Cell}) 
@@ -8,32 +22,77 @@ export class MainContract implements Contract{
     
     }
 
-    static createFromConfig(config: any, code: Cell, workchain = 0) {
-        const data = beginCell().endCell();
+    static createFromConfig(config: MainContractConfig, code: Cell, workchain = 0) {
+        const data = mainContractConfigToCell(config);
         const init = {code, data};
         const address = contractAddress(workchain, init);
         return new MainContract(address, init);
     }
 
-    async sendInternalMessage(provider: ContractProvider, sender: Sender, value: bigint) 
-    {
+    async sendIncrement(provider: ContractProvider, sender: Sender, value: bigint, increment_by: number) {
+        const msg_body = beginCell().storeUint(1, 32).storeUint(increment_by, 32).endCell();
+
         await provider.internal(
             sender, 
             {
                 value, 
                 sendMode: SendMode.PAY_GAS_SEPARATELY,
-                body: beginCell().endCell()
+                body: msg_body
             }
         );
     }
 
+    async sendDeposit(provier: ContractProvider, sender: Sender, value: bigint) {
+        const msg_body = beginCell()
+        .storeUint(2, 32) // OP code
+        .endCell();
+
+        await provier.internal(sender, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msg_body
+        });
+    }
+
+    async sendNoCodeDeposit(provider: ContractProvider, sender: Sender, value: bigint) {
+        const msg_body = beginCell().endCell();
+        await provider.internal(sender, {
+            value, 
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msg_body
+        });
+    }
+
+    async sendWithdrawalRequest(provider: ContractProvider, sender: Sender, value: bigint, amount: bigint) {
+        const msg_body = beginCell()
+        .storeUint(3, 32) // OP code
+        .storeCoins(amount)
+        .endCell();
+    
+        await provider.internal(sender, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msg_body
+        });
+
+    }
+
+
     async getData(provider: ContractProvider) {
-        const { stack } = await provider.get("get_the_latest_sender", []);
+        const { stack } = await provider.get("get_contract_storage_data", []);
         return {
+            number: stack.readNumber(),
             recent_sender: stack.readAddress(),
+            owner_address: stack.readAddress(),
         };
     }
 
+    async getBalance(provider: ContractProvider) {
+        const {stack} = await provider.get("balance", []);
+        return {
+            balance: stack.readNumber()
+        };
+    }
 };
 
 
